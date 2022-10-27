9f0bf39b271f24c7a8bd4ab979f744ba44df6309
John Chiang <o926.428.377@gmail.com>
2022-10-27 10:18:21 +0800

    Update kktix.js

--- kktix.js
+++ kktix.js
@@ -1,84 +1,6 @@
-function POST(uri, post_body, i=1){
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        xhr.open("POST", uri, true);
-        xhr.setRequestHeader('accept','application/json, text/plain, */*');
-        xhr.setRequestHeader('accept-language','zh-TW,zh;q=0.9');
-        xhr.setRequestHeader('content-type','text/plain');
-        xhr.onreadystatechange = async function() {
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                var body = JSON.parse(xhr.responseText);
-                if (body.result === "Enqueue too soon!") {
-                    setTimeout(function(){
-                        POST(uri, post_body, i+1).then(function (result) {
-                            resolve(result);
-                        });
-                    }, 2000*i);
-                } else {
-                    resolve(body);
-                }
-            } else if (xhr.status == 429) {
-                setTimeout(function(){
-                    POST(uri, post_body, i+1).then(function (result) {
-                        resolve(result);
-                    });
-                }, 2000*i);
-            } else if (xhr.status == 500) {
-                reject(false);
-            }
-        }
-        xhr.send(JSON.stringify(post_body));
-    })
-}
 
-function GET(uri){
-    return new Promise(function (resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        xhr.open("GET", uri, true);
-        xhr.setRequestHeader('accept','application/json, text/plain, */*');
-        xhr.setRequestHeader('accept-language','zh-TW,zh;q=0.9');
-        xhr.setRequestHeader('content-type','text/plain');
-        xhr.onreadystatechange = async function() {
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                var body = JSON.parse(xhr.responseText);
-                if (body.result === "not_found") {
-                    GET(uri).then(function (result) {
-                        resolve(result);
-                    });
-                } else {
-                    resolve(body);
-                }
-            } else if (xhr.status == 500) {
-                reject(false);
-            }
-        }
-        xhr.send();
-    })
-}
-
-function GET_token(uri, idx){
-    return new Promise(function (resolve, reject) {
-        var re = /<meta name="csrf-token" content="([^"]*)" \/>/i;
-        var xhr = new XMLHttpRequest();
-        xhr.withCredentials = true;
-        xhr.open("GET", uri, true);
-        xhr.setRequestHeader('accept-language','zh-TW,zh;q=0.9');
-        xhr.setRequestHeader('content-type','text/plain');
-        xhr.onreadystatechange = async function() {
-            if(xhr.readyState == 4 && xhr.status == 200) {
-                var token = xhr.responseText.match(re)[1];
-                resolve({authenticity_token:token, idx:idx});
-            } else if (xhr.status == 500) {
-                reject(false);
-            }
-        }
-        xhr.send();
-    })
-}
-
-function ticket_grabbing(event_name, custom_captcha, expect_price, quantity) {
+function ticket_grabbing_master(event_name, custom_captcha, expect_price, quantity) {
+    window.open(`https://kktix.com/events/${event_name}/registrations/new`, "_self");
     var base_info_url = `https://kktix.com/g/events/${event_name}/base_info`;
     var data = {"tickets":[{"id":431420,"quantity":2,"invitationCodes":[],"member_code":"","use_qualification_id":null}],"currency":"TWD","recaptcha":{},"custom_captcha":custom_captcha,"agreeTerm":true};
     data.tickets[0].quantity = quantity;
@@ -102,6 +24,16 @@ function ticket_grabbing(event_name, custom_captcha, expect_price, quantity) {
             }
         }
     });
+}
+
+function ticket_grabbing(event_name, custom_captcha, expect_price, quantity) {
+    var xx = window.open(`https://kktix.com/events/${event_name}/registrations/new`);
+    var script = document.createElement('script');
+    script.src = 'https://o926428377.github.io/share_enc_text/kktix.js';
+    xx.document.head.appendChild(script);
+    var exec = document.createElement('script');
+    exec.append(`window.onload=function(){ticket_grabbing_master(${event_name}, ${custom_captcha}, ${expect_price},${quantity});}`)
+    xx.document.head.appendChild(exec);
 }
 
 /*
